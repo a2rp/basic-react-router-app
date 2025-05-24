3f240af0105bccd0c597a31195bc4ac0b5289e51
ash1198 <ash.ranjan09@gmail.com>
2025-05-24 13:01:30 +0530

    added scroll to top functionality on route change

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Header from './components/header'
 import Footer from './components/footer'
 import styled from 'styled-components'
 import AppRoutes from './appRoutes'
+import ScrollToTop from './components/ScrollToTop'
 
 const App = () => {
     return (
         <>
             <Header />
             <Main>
+                <ScrollToTop />
                 <AppRoutes />
             </Main>
             <Footer />
